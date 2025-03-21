702ea00bc62e4cc4452ce93883d0e43d1c9979cc
gennaro-97 <58686645+gennaro-97@users.noreply.github.com>
2025-03-21 10:51:00 +0100

    risolto RestController

--- backend/src/main/java/com/example/backend/controllers/PiattoController.java
+++ backend/src/main/java/com/example/backend/controllers/PiattoController.java
@@ -13,13 +13,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 
-@Controller
+@RestController
 @RequestMapping("/piatti")
 @RequiredArgsConstructor
 public class PiattoController {
